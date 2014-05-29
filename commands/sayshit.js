384b3e6a423f79675ed5c2493ded048a3dfb1c8c
James Thomas <james.thomas@codethink.co.uk>
2014-05-29 13:46:30 +0100

    Make sure funnynickname knows he's good

--- commands/sayshit.js
+++ commands/sayshit.js
@@ -9,7 +9,11 @@ var SayShit = Command.define(module, {
 SayShit.prototype.postMessageHook = function(nick, text, message) {
     for (var i=0; i<this.data.length; i++) {
         var s = this.data[i];
-        if (text.match(s.regex)) {
+        var nickMatch = s.nick ? false : true;
+        if (!nickMatch) {
+            nickMatch = s.nick===nick ? true : false;
+        }
+        if (text.match(s.regex) && nickMatch) {
             if (Math.random() < s.probability) {
                 var shitIndex = parseInt(Math.random()*s.shit.length);
                 this.bot.say(this.options.chan, s.shit[shitIndex]);
